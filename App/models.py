cafd0b81dbe869b89b3787168479b70315b4086c
Kerkko Pelttari <kerkko@robocorp.com>
2021-09-17 16:54:56 +0300

    Permit longer string for User.password field

--- App/models.py
+++ App/models.py
@@ -7,7 +7,7 @@ def initialize_models(db: SQLAlchemy):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(80), unique=True, nullable=False)
         email = db.Column(db.String(120), unique=True, nullable=False)
-        password = db.Column(db.String(64), unique=True, nullable=False)
+        password = db.Column(db.String(1024), unique=True, nullable=False)
 
         def __repr__(self):
             return '<User %r>' % self.username
