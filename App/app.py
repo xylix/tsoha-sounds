2c23954d01bdd611d1b39de6f0e41671a7750cc4
Kerkko Pelttari <kerkko@robocorp.com>
2021-09-20 12:29:48 +0300

    Start blackening and pylinting

--- App/app.py
+++ App/app.py
@@ -15,7 +15,7 @@ from . import models
 app = Flask(__name__)
 
 # Heroku provides an old postgres format which doesn't work with new SQLAlchemy
-REWRITTEN_DATABASE_URL = getenv("DATABASE_URL").replace('postgres://', 'postgresql://')
+REWRITTEN_DATABASE_URL = getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 app.config["SQLALCHEMY_DATABASE_URI"] = REWRITTEN_DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
@@ -23,20 +23,22 @@ db = SQLAlchemy(app)
 models = models.initialize_models(db)
 
 
-def logged_in_user():
+def logged_in_user() -> bool:
     if not session.get("user_id"):
         # There is no active session
         return False
-    
+
     sql = "SELECT id FROM AppUsers WHERE id=:id"
-    if not db.session.execute(sql, {"id":session.get("user_id")}).first():
+    if not db.session.execute(sql, {"id": session.get("user_id")}).first():
         # Session has expired or comes from older database instance, delete it
         logout()
         return False
     return True
 
+
 def is_admin():
-    return session.get("is_admin") == True
+    # FIXME: if the session["is_admin"] can be a string like "0" add `is True` comparison
+    return session.get("is_admin")
 
 
 def auth_required(f):
@@ -50,6 +52,7 @@ def auth_required(f):
         if not allow:
             return render_template("error.html", error="Ei oikeutta nähdä sivua")
         return f(*args, **kws)
+
     return decorated_function
 
 
@@ -59,12 +62,22 @@ def index():
         return render_template("index.html")
     # FIXME: combine the queries
     if is_admin():
-        result = db.session.execute("SELECT name,id FROM Projects WHERE NOT owner = :user_id", {"user_id": session["user_id"]})
+        result = db.session.execute(
+            "SELECT name,id FROM Projects WHERE NOT owner = :user_id",
+            {"user_id": session["user_id"]},
+        )
     else:
-        result = db.session.execute("SELECT name,id FROM Projects WHERE NOT owner = :user_id AND published=True", {"user_id": session["user_id"]})
+        result = db.session.execute(
+            "SELECT name,id FROM Projects WHERE NOT owner = :user_id AND published=True",
+            {"user_id": session["user_id"]},
+        )
     public_projects = result.fetchall()
-    own_projects = db.session.execute("SELECT name,id FROM Projects WHERE owner=:id", {"id": session["user_id"]}).fetchall()
-    return render_template("index.html", own_projects=own_projects, public_projects=public_projects) 
+    own_projects = db.session.execute(
+        "SELECT name,id FROM Projects WHERE owner=:id", {"id": session["user_id"]}
+    ).fetchall()
+    return render_template(
+        "index.html", own_projects=own_projects, public_projects=public_projects
+    )
 
 
 @app.route("/project/<int:id>")
@@ -74,7 +87,7 @@ def project(id: int):
     sql = "SELECT id, name, owner, published FROM Projects WHERE id=:id"
     result = db.session.execute(sql, {"id": id})
     project_info = result.fetchone()
-    
+
     files_sql = """SELECT f.id, f.data, f.name
         FROM Files f
         JOIN FileProject fp ON f.id = fp.file_id
@@ -91,7 +104,16 @@ def project(id: int):
         WHERE containing_project=:id"""
     result = db.session.execute(comments_sql, {"id": id})
     comments = result.fetchall()
-    return render_template("project_view.html", name=project_info.name, project_id=project_info.id, current_files=files, published=project_info.published, comments=comments, is_owner=project_info.owner==session['user_id'])
+    allowed_to_modify = project_info.owner == session["user_id"] or is_admin()
+    return render_template(
+        "project_view.html",
+        name=project_info.name,
+        project_id=project_info.id,
+        current_files=files,
+        published=project_info.published,
+        comments=comments,
+        allowed_to_modify=allowed_to_modify,
+    )
 
 
 @app.route("/project/<int:id>/send_publish", methods=["POST"])
@@ -102,8 +124,11 @@ def send_publish(id: int):
     sql = "UPDATE Projects SET published=:published WHERE id=:id"
     result = db.session.execute(sql, {"id": id, "published": published})
     db.session.commit()
-    print(f"Updated published status for project {id}, new value of publish: {published}")
+    print(
+        f"Updated published status for project {id}, new value of publish: {published}"
+    )
     return redirect(f"/project/{id}")
+
 
 # TODO: Does this benefit from ID since it offers change to add file to any project?
 @app.route("/add_file/<int:id>")
@@ -111,17 +136,20 @@ def send_publish(id: int):
 def add_file(id):
     sql = "SELECT id, name FROM Projects WHERE owner=:user_id"
     user_id = session["user_id"]
-    result = db.session.execute(sql, {"user_id":user_id})
+    result = db.session.execute(sql, {"user_id": user_id})
     available_projects = result.fetchall()
-    
+
     available_files = db.session.execute("SELECT id,name FROM Files").fetchall()
-    return render_template("new_file.html", available_projects=available_projects, available_files=available_files)
+    return render_template(
+        "new_file.html",
+        available_projects=available_projects,
+        available_files=available_files,
+    )
 
 
 @app.route("/send_file", methods=["POST"])
 @auth_required
 def send_file():
-    # FIXME: could we validate here not to send file from same project to same project?
     new_file: Optional[FileStorage] = request.files.get("new_file")
     old_file = request.form.get("old_file")
     project = request.form["project"]
@@ -132,14 +160,24 @@ def send_file():
         assert new_file
 
         sql = "INSERT INTO Files(owner, data, name) VALUES (:owner, :data, :name) RETURNING id"
-        files_result = db.session.execute(sql, {"owner":session["user_id"], "data":new_file.stream.read(), "name":new_file.filename})
+        files_result = db.session.execute(
+            sql,
+            {
+                "owner": session["user_id"],
+                "data": new_file.stream.read(),
+                "name": new_file.filename,
+            },
+        )
 
         sql = "INSERT INTO FileProject(file_id, project_id) VALUES (:file_id, :project_id)"
-        result = db.session.execute(sql, {"file_id": files_result.first().id, "project_id": project})
+        result = db.session.execute(
+            sql, {"file_id": files_result.first().id, "project_id": project}
+        )
         db.session.commit()
         return redirect(f"/project/{project}")
 
     if old_file:
+        # FIXME: could we validate here not to send file from same project to same project?
         print("Appending existing file")
 
         sql = "INSERT INTO FileProject(file_id, project_id) VALUES (:file_id, :project_id)"
@@ -162,14 +200,16 @@ def send_project():
     name = request.form["name"]
     try:
         sql = "INSERT INTO Projects (name, owner) VALUES (:name, :owner)"
-        result = db.session.execute(sql, {"name":name, "owner": session["user_id"]})
+        result = db.session.execute(sql, {"name": name, "owner": session["user_id"]})
         db.session.commit()
         print(f"Succesfully created project {name}")
         return redirect("/")
     except BaseException as e:
         print(e)
         # FIXME: this could be any db error
-        return render_template("error.html", error="Please select an unique name for your projct")
+        return render_template(
+            "error.html", error="Please select an unique name for your projct"
+        )
 
 
 @app.route("/query_project", methods=["GET"])
@@ -177,7 +217,7 @@ def send_project():
 def query_project():
     query = request.args["query"]
     sql = "SELECT id, name FROM Projects WHERE name LIKE :query AND published "
-    result = db.session.execute(sql, {"query":"%"+query+"%"})
+    result = db.session.execute(sql, {"query": "%" + query + "%"})
     projects = result.fetchall()
     return render_template("search_results.html", projects=projects)
 
@@ -187,19 +227,22 @@ def query_project():
 def send_comment(id: int):
     content = request.form["comment"]
     sql = "INSERT INTO Comments(sender, containing_project, content) VALUES (:sender,:containing_project,:content)"
-    result = db.session.execute(sql, {"sender":session["user_id"], "containing_project":id, "content":content })
+    result = db.session.execute(
+        sql,
+        {"sender": session["user_id"], "containing_project": id, "content": content},
+    )
     db.session.commit()
     return redirect(f"/project/{id}")
 
 
-@app.route("/login",methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
     print(f"Querying for user with username: {username}")
     # Check username and password
     sql = "SELECT id, password, is_admin FROM AppUsers WHERE username=:username"
-    result = db.session.execute(sql, {"username":username})
+    result = db.session.execute(sql, {"username": username})
     user = result.first()
     if not user:
         print(f"User not found, result of query: {user}")
@@ -217,16 +260,17 @@ def login():
     return redirect("/")
 
 
-
 @app.route("/logout")
 def logout():
     del session["username"]
     del session["user_id"]
     return redirect("/")
 
+
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/send_register", methods=["POST"])
 def send_register():
@@ -235,12 +279,15 @@ def send_register():
     password = generate_password_hash(request.form["password"])
     try:
         sql = "INSERT INTO AppUsers(username, password,email) VALUES (:username, :password, :email)"
-        result = db.session.execute(sql, {"username":username, "password": password, "email": email})
+        result = db.session.execute(
+            sql, {"username": username, "password": password, "email": email}
+        )
         db.session.commit()
         return redirect("/")
     except BaseException as e:
         print(e)
         return render_template("error.html", error="Please select an unique username")
+
 
 if __name__ == "__main__":
     app.run()
--- App/models.py
+++ App/models.py
@@ -1,13 +1,24 @@
-from sqlalchemy import Column, Integer, ForeignKey, func, DateTime, String, Boolean, LargeBinary
+from sqlalchemy import (
+    Column,
+    Integer,
+    ForeignKey,
+    func,
+    DateTime,
+    String,
+    Boolean,
+    LargeBinary,
+)
 from sqlalchemy.schema import CreateTable
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
 
 
 def initialize_models(db: SQLAlchemy):
-    file_project_association_table = db.Table('FileProject', db.metadata,
-        Column('file_id', ForeignKey('Files.id'), primary_key=True),
-        Column('project_id', ForeignKey('Projects.id'), primary_key=True),
+    file_project_association_table = db.Table(
+        "FileProject",
+        db.metadata,
+        Column("file_id", ForeignKey("Files.id"), primary_key=True),
+        Column("project_id", ForeignKey("Projects.id"), primary_key=True),
     )
 
     class AppUser(db.Model):
@@ -19,12 +30,12 @@ def initialize_models(db: SQLAlchemy):
         is_admin = Column(Boolean, default=False)
 
         def __repr__(self):
-            return '<User %r>' % self.username
+            return "<User %r>" % self.username
 
     class Project(db.Model):
         __tablename__ = "Projects"
         id = Column(Integer, primary_key=True)
-        owner = Column(ForeignKey('AppUsers.id'))
+        owner = Column(ForeignKey("AppUsers.id"))
         name = Column(String(80), unique=True)
         published = Column(Boolean, default=False)
         files = db.relationship("File", secondary=file_project_association_table)
@@ -32,33 +43,46 @@ def initialize_models(db: SQLAlchemy):
     class File(db.Model):
         __tablename__ = "Files"
         id = Column(Integer, primary_key=True)
-        owner = Column(ForeignKey('AppUsers.id'))
+        owner = Column(ForeignKey("AppUsers.id"))
         data = Column(LargeBinary)
         name = Column(String(80))
 
     class Comment(db.Model):
         __tablename__ = "Comments"
         id = Column(Integer, primary_key=True)
-        sender = Column(ForeignKey('AppUsers.id'))
-        containing_project = Column(ForeignKey('Projects.id'))
+        sender = Column(ForeignKey("AppUsers.id"))
+        containing_project = Column(ForeignKey("Projects.id"))
         content = Column(String(1024), nullable=False)
-        sent = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    
+        sent = Column(
+            DateTime(timezone=True), server_default=func.now(), nullable=False
+        )
+
     db.create_all()
 
     if AppUser.query.filter_by(username="admin").first() is None:
-        admin_user = AppUser(username='admin', email='admin@/dev/null', password=generate_password_hash('root'), is_admin=True)
+        admin_user = AppUser(
+            username="admin",
+            email="admin@/dev/null",
+            password=generate_password_hash("root"),
+            is_admin=True,
+        )
         db.session.add(admin_user)
         db.session.commit()
-        sample_project = Project(owner=admin_user.id, name="First public project", published=True)
+        sample_project = Project(
+            owner=admin_user.id, name="First public project", published=True
+        )
         sample_file = File(owner=admin_user.id, name="Test file")
         sample_project.files.append(sample_file)
         db.session.add(sample_project)
         db.session.commit()
-        sample_comment = Comment(sender=admin_user.id, containing_project=sample_project.id, content="First test comment")
+        sample_comment = Comment(
+            sender=admin_user.id,
+            containing_project=sample_project.id,
+            content="First test comment",
+        )
         db.session.add(sample_comment)
         db.session.commit()
-    
+
     print(db.engine.table_names())
     models = AppUser, Project, File, Comment
     print(CreateTable(file_project_association_table))
